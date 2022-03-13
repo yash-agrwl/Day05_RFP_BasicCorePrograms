bbb41b6f9f3ef2c47b824a4570dd9056efaae082
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-13 07:43:47 +0530

    added empty project file with welcome message

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Computation Of Basic Core Programs.\n");
+        }
+    }
+}
